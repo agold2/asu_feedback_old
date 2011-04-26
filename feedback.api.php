d54d3d5c3e140d334c17cf6a46f2edbd036c9b86
Jody Hamilton <jody@zivtech.com>
2011-04-26 11:12:16 -0400

    #351698 by Jody Lynn and sun, feedback is now a fieldable entity!

--- feedback.api.php
+++ feedback.api.php
@@ -24,6 +24,19 @@ function hook_feedback_load($entries) {
 }
 
 /**
+ * Act on a feedback entry before it is saved.
+ *
+ * Modules implementing this hook can act on the feedback entry object before it
+ * is inserted or updated.
+ *
+ * @param $entry
+ *   The feedback entry object.
+ */
+function hook_feedback_presave($entry) {
+  $entry->foo = 'bar';
+}
+
+/**
  * Respond to creation of a new feedback entry.
  *
  * @param $entry
@@ -67,6 +80,52 @@ function hook_feedback_delete($entry) {
   db_delete('mytable')
     ->condition('fid', $entry->fid)
     ->execute();
+}
+
+/**
+ * The feedback entry is being displayed.
+ *
+ * The module should format its custom additions for display and add them to the
+ * $entry->content array.
+ *
+ * @param $entry
+ *   The feedback entry object.
+ * @param $view_mode
+ *   View mode, e.g. 'full'.
+ * @param $langcode
+ *   The language code used for rendering.
+ *
+ * @see hook_feedback_view_alter()
+ * @see hook_entity_view()
+ */
+function hook_feedback_view($entry, $view_mode, $langcode) {
+  $entry->content['foo'] = array(
+    '#markup' => t('Bar'),
+  );
+}
+
+/**
+ * The feedback entry was built; the module may modify the structured content.
+ *
+ * This hook is called after the content has been assembled in a structured
+ * array and may be used for doing processing which requires that the complete
+ * content structure has been built.
+ *
+ * @param $build
+ *   A renderable array representing the feedback entry.
+ *
+ * @see feedback_view()
+ * @see hook_entity_view_alter()
+ */
+function hook_feedback_view_alter(&$build) {
+  // Check for the existence of a field added by another module.
+  if (isset($build['an_additional_field'])) {
+    // Change its weight.
+    $build['an_additional_field']['#weight'] = -10;
+  }
+
+  // Add a #post_render callback to act on the rendered HTML of the entry.
+  $build['#post_render'][] = 'my_module_feedback_post_render';
 }
 
 /**
